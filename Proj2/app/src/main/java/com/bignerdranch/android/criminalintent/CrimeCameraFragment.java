8fc2189af3f8b5a008b203a88097d2d828c3fe9b
Zach Arnold <zparnold@wpi.edu>
2015-03-31 15:16:48 -0400

    last set of changes before I test it

--- Proj2/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ Proj2/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -62,13 +62,12 @@ public class CrimeCameraFragment extends Fragment {
             
             if (success) {
                 // set the photo filename on the result intent
-                if (success) {
                     Intent i = new Intent();
                     i.putExtra(EXTRA_PHOTO_FILENAME, filename);
                     getActivity().setResult(Activity.RESULT_OK, i);
-                } else {
-                    getActivity().setResult(Activity.RESULT_CANCELED);
-                }
+            }
+            else {
+                getActivity().setResult(Activity.RESULT_CANCELED);
             }
             getActivity().finish();
         }
