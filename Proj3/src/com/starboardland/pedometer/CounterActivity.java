2afaaf03387edd74dc4cbe5f11f509cb4f5e48c5
Zach Arnold <zparnold@wpi.edu>
2015-04-04 17:26:04 -0400

    adds map activity

--- Proj3/src/com/starboardland/pedometer/CounterActivity.java
+++ Proj3/src/com/starboardland/pedometer/CounterActivity.java
@@ -1,25 +1,45 @@
 package com.starboardland.pedometer;
 
-import android.app.Activity;
 import android.content.Context;
 import android.hardware.*;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CounterActivity extends Activity implements SensorEventListener {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Date;
+import java.util.Timer;
+
+public class CounterActivity extends FragmentActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private TextView count;
+    private TextView minCount;
     boolean activityRunning;
+    private int minuteCount;
+    private static final int ONE_MINUTE = 60000;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         count = (TextView) findViewById(R.id.count);
+        setMinuteCount(0);
 
+        //setup new recuring task each minute
+        MinuteCountIncrementTask tt = new MinuteCountIncrementTask(this);
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(tt,new Date(),ONE_MINUTE);
+
+        setMinCount((TextView) findViewById(R.id.minCount));
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        setUpMapIfNeeded();
     }
 
     @Override
@@ -32,7 +52,46 @@ public class CounterActivity extends Activity implements SensorEventListener {
         } else {
             Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
         }
+        setUpMapIfNeeded();
 
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
     @Override
@@ -53,5 +112,21 @@ public class CounterActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public int getMinuteCount() {
+        return minuteCount;
+    }
+
+    public void setMinuteCount(int minuteCount) {
+        this.minuteCount = minuteCount;
+    }
+
+    public TextView getMinCount() {
+        return minCount;
+    }
+
+    public void setMinCount(TextView minCount) {
+        this.minCount = minCount;
     }
 }
---
+++ Proj3/src/com/starboardland/pedometer/MinuteCountIncrementTask.java
@@ -0,0 +1,28 @@
+package com.starboardland.pedometer;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+import java.util.TimerTask;
+
+/**
+ * Created by Zach on 4/4/2015.
+ */
+public class MinuteCountIncrementTask extends TimerTask {
+    private CounterActivity activity;
+
+    public  MinuteCountIncrementTask(CounterActivity A){
+        this.activity = A;
+    }
+    /**
+     *
+     */
+    @Override
+    public void run() {
+        int count = this.activity.getMinuteCount();
+        count++;
+        TextView tvMinCount = activity.getMinCount();
+        
+        this.activity.setMinuteCount(count);
+    }
+}
