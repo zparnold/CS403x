2228bd3cb6742e658eeaf96ce532a48d7f8fb881
Zach Arnold <zparnold@wpi.edu>
2015-04-19 17:58:02 -0400

    adds final project

---
+++ Proj4/app/src/androidTest/java/edu/wpi/cs/cs403xproj4/wpifreebies/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs.cs403xproj4.wpifreebies;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
